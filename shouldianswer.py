78577381c943719c1ae2cb0e6f7e0f0c4fc13aaf
Swyter <swyterzone@gmail.com>
2021-10-03 19:51:39 +0200

    Make sure the Python parser exits alright. We can go from there.

--- shouldianswer.py
+++ shouldianswer.py
@@ -15,13 +15,14 @@ else:
 
 
 # swy: here's the real meat
-def parse_data_slice(file_path):
+def parse_data_slice(needed_magic, file_path):
     with open(file_path, 'rb+') as f:
         magic = struct.unpack('4s', f.read(4))[0]
-        f.seek(0x54)
-        fourc = struct.unpack('4s', f.read(4))[0];
-
-
+        print(magic)
+        if not magic == needed_magic:
+            return None
+        #f.seek(0x54)
+        #fourc = struct.unpack('4s', f.read(4))[0];
 
 try:
     from pathlib import Path
@@ -35,14 +36,22 @@ except: # swy: python 2 does not include pathlib by default, so fallback to this
     for root, dirnames, filenames in os.walk(sda_folder):
         for filename in fnmatch.filter(filenames, 'data_slice_*.dat'):
             sda_glob.append(os.path.join(root, filename))
-            
+
+data = []
+
 # swy: aggregate the bundled application data, each number is a regional prefix
 for file_path in sda_glob:
-    parse_data_slice('MTZF', file_path)
+    print(" [>] opening %s" % file_path)
+
+    cur = parse_data_slice('MTZF', file_path)
+    if cur:
+        data += cur 
+
+    break
 
 # swy: apply the downloaded update; hopefully the data will be fresher this way
-update = parse_data_slice('MTZD', "data_slice_downloaded_update.bin")
+data_update = parse_data_slice('MTZD', "data_slice_downloaded_update.bin")
 
 
 # swy: dump the result
-print("\n".join(data.entries))
+print("\n".join(data))
