972b06cadd46ca5c87c094033f3437da6d86c912
Swyter <swyterzone@gmail.com>
2021-10-03 21:17:09 +0200

    Thing the thong. I can't believe we need this stuff..

--- .generate_call_blocklist.sh
+++ .generate_call_blocklist.sh
@@ -1,3 +1,5 @@
+chmod +x shouldianswer.py
+
 curl --header 'Accept: application/json' --header 'Authorization: Basic Y2RhbmQ6djFna3dpamhzbmdyZ29pNjh6c2Z3Mzh2' 'https://ws.cleverdialer.com/api/1.3/spam?region=ES' -v -o cleverdialer.json
 jq .[].normalizedPhone -r cleverdialer.json > blocklist_unsorted.txt
 sort blocklist_unsorted.txt > blocklist.txt
