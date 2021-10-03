54afebfe9a7346b997cfdce683adb231832674c4
Swyter <swyterzone@gmail.com>
2021-10-03 18:22:42 +0200

    More remaining binary template improvements. More script tweaking.

--- .generate_call_blocklist.sh
+++ .generate_call_blocklist.sh
@@ -2,6 +2,5 @@ curl --header 'Accept: application/json' --header 'Authorization: Basic Y2RhbmQ6
 jq .[].normalizedPhone -r cleverdialer.json > blocklist_unsorted.txt
 sort blocklist_unsorted.txt > blocklist.txt
 
-# https://web.archive.org/web/20211003104644if_/http://download.shouldianswer.net/download/shouldianswer_obsolete.apk
-curl --compressed -o dataslice.bin.gz 'https://srv1.shouldianswer.net/srv2/get-database2?v=6&appver=11014&dbver=1381'
-gunzip dataslice.bin.gz
+curl -LOJ http://download.shouldianswer.net/download/shouldianswer_obsolete.apk # https://web.archive.org/web/20211003104644if_/http://download.shouldianswer.net/download/shouldianswer_obsolete.apk
+curl --compressed -o dataslice.bin.gz 'https://srv1.shouldianswer.net/srv2/get-database2?v=6&appver=11014&dbver=1381' && gunzip --force dataslice.bin.gz
