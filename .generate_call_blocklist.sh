541053e29f8db9de822fc5b41ba8d51264bb6d1b
Swyter <swyterzone@gmail.com>
2021-10-03 13:14:39 +0200

    Add the rating database update url for ShouldIAnswer, seems like they gzip the response, well done.

--- .generate_call_blocklist.sh
+++ .generate_call_blocklist.sh
@@ -1,3 +1,6 @@
 curl --header 'Accept: application/json' --header 'Authorization: Basic Y2RhbmQ6djFna3dpamhzbmdyZ29pNjh6c2Z3Mzh2' 'https://ws.cleverdialer.com/api/1.3/spam?region=ES' -v -o cleverdialer.json
 jq .[].normalizedPhone -r cleverdialer.json > blocklist_unsorted.txt
 sort blocklist_unsorted.txt > blocklist.txt
+
+
+curl -o thing.bin 'https://srv1.shouldianswer.net/srv2/get-database2?v=6&appver=11014&dbver=1381'
