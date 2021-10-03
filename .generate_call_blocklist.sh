289e77c3e27f9a9a5846e29568a6d62124489458
Swyter <swyterzone@gmail.com>
2021-10-03 18:37:31 +0200

    Remove the development files from the new blocklist branch.

--- .generate_call_blocklist.sh
+++
@@ -1,6 +0,0 @@
-curl --header 'Accept: application/json' --header 'Authorization: Basic Y2RhbmQ6djFna3dpamhzbmdyZ29pNjh6c2Z3Mzh2' 'https://ws.cleverdialer.com/api/1.3/spam?region=ES' -v -o cleverdialer.json
-jq .[].normalizedPhone -r cleverdialer.json > blocklist_unsorted.txt
-sort blocklist_unsorted.txt > blocklist.txt
-
-curl -LOJ http://download.shouldianswer.net/download/shouldianswer_obsolete.apk # https://web.archive.org/web/20211003104644if_/http://download.shouldianswer.net/download/shouldianswer_obsolete.apk
-curl --compressed -o dataslice.bin.gz 'https://srv1.shouldianswer.net/srv2/get-database2?v=6&appver=11014&dbver=1381' && gunzip --force dataslice.bin.gz
