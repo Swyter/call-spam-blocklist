da25e070ff02bb44584731d4b1199acc81f68525
Swyter <swyterzone@gmail.com>
2021-10-03 19:23:50 +0200

    Fix the second blocklist download and setup.

--- .generate_call_blocklist.sh
+++ .generate_call_blocklist.sh
@@ -2,5 +2,7 @@ curl --header 'Accept: application/json' --header 'Authorization: Basic Y2RhbmQ6
 jq .[].normalizedPhone -r cleverdialer.json > blocklist_unsorted.txt
 sort blocklist_unsorted.txt > blocklist.txt
 
-curl -LOJ http://download.shouldianswer.net/download/shouldianswer_obsolete.apk # https://web.archive.org/web/20211003104644if_/http://download.shouldianswer.net/download/shouldianswer_obsolete.apk
-curl --compressed -o dataslice.bin.gz 'https://srv1.shouldianswer.net/srv2/get-database2?v=6&appver=11014&dbver=1381' && gunzip --force dataslice.bin.gz
+curl -LOJ 'http://download.shouldianswer.net/download/shouldianswer_obsolete.apk' && unzip -n shouldianswer_obsolete.apk -d /tmp/apk && cp --no-clobber /tmp/apk/assets/data_slice_*.dat . # https://web.archive.org/web/20211003104644if_/http://download.shouldianswer.net/download/shouldianswer_obsolete.apk
+curl --compressed -o data_slice_downloaded_update.bin.gz 'https://srv1.shouldianswer.net/srv2/get-database2?v=6&appver=11014&dbver=1381' && gunzip --force data_slice_downloaded_update.bin.gz
+
+ls -lash
